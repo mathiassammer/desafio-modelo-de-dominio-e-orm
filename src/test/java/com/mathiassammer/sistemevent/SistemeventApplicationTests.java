929431749a9122a7f6d9e74326196b53c5139d51
Mathias Sammer <mathias.sammer.dev@gmail.com>
2026-02-17 10:48:40 -0300

    first commit

---
+++ src/test/java/com/mathiassammer/sistemevent/SistemeventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathiassammer.sistemevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemeventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
