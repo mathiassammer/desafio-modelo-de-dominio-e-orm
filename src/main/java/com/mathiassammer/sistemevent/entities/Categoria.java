effa481a2343853186f115cf134e1a678a2f2dad
Mathias Sammer <mathias.sammer.dev@gmail.com>
2026-02-17 11:35:40 -0300

    feat: create entities Categoria and Atividade

---
+++ src/main/java/com/mathiassammer/sistemevent/entities/Categoria.java
@@ -0,0 +1,64 @@
+package com.mathiassammer.sistemevent.entities;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Atividade> atividades = new ArrayList<>();
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Atividade> getAtividades() {
+        return atividades;
+    }
+}
