97806e6cb05893f477bc22fc21c5ad62348a3c33
Mathias Sammer <mathias.sammer.dev@gmail.com>
2026-02-17 14:07:25 -0300

    feat: create entity Bloco

--- src/main/java/com/mathiassammer/sistemevent/entities/Atividade.java
+++ src/main/java/com/mathiassammer/sistemevent/entities/Atividade.java
@@ -2,7 +2,9 @@ package com.mathiassammer.sistemevent.entities;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_atividade")
@@ -19,6 +21,9 @@ public class Atividade {
     @ManyToOne
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
+
+    @OneToMany(mappedBy = "atividade")
+    private Set<Bloco> blocos = new HashSet<>();
 
     public Atividade() {
     }
@@ -83,5 +88,9 @@ public class Atividade {
 
     public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
+    }
+
+    public Set<Bloco> getBlocos() {
+        return blocos;
     }
 }
